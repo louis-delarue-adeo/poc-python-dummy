819bfb6f8c248f2ea47e5cf36cb7ab0e9e77d889
louis-delarue-adeo <louis.delarue@ext.adeo.com>
2022-09-15 14:25:00 +0200

    First commit

---
+++ main.py
@@ -0,0 +1,12 @@
+import uvicorn
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", port=8000, reload=True, log_level="info")
